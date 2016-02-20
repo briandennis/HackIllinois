7ca3cc50cbfef900d22393d8501e10302421d2ee
Brian Dennis <briandennis321@gmail.com>
2016-02-20 14:09:17 -0600

    added event function and new templating syntax

--- templating/templating.ts
+++ templating/templating.ts
@@ -3,14 +3,23 @@ import {secondComponent} from './secondComponent';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>My Component </h1>
+  template: `<h1 [textContent]='getName()'></h1>
              <h2>{{numUsers}}</h2>
              <second-component></second-component>
+             <button (click)='buttonClicked()'>Click me!</button>
              `,
   directives: [secondComponent]
 })
 export class myApp {
 
   numUsers: number = 72;
+
+  getName (){
+    return 'Brian';
+  }
+
+  buttonClicked(){
+    alert('Way to follow directions!');
+  }
 
 }
