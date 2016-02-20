b3e277460a08ca27c7e290b3e3b135184535be1e
Brian Dennis <briandennis321@gmail.com>
2016-02-20 04:32:01 -0600

    added second component on templates project

---
+++ templating/secondComponent.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'second-component',
+  template: `
+    <h3>Second Component Test </h3>
+  `
+})
+export class secondComponent{
+
+}
--- templating/templating.ts
+++ templating/templating.ts
@@ -1,9 +1,13 @@
 import {Component} from 'angular2/core';
+import {secondComponent} from './secondComponent';
 
 @Component({
   selector: 'my-app',
   template: `<h1>My Component </h1>
-             <h2>{{numUsers}}</h2>`
+             <h2>{{numUsers}}</h2>
+             <second-component></second-component>
+             `,
+  directives: [secondComponent]
 })
 export class myApp {
 
