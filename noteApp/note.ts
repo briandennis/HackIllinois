30bd1a8b67bf7359f79f71263872c717a5994423
Brian Dennis <briandennis321@gmail.com>
2016-02-20 23:16:20 -0600

    added text edit features

---
+++ noteApp/note.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'na-note',
+  template: `
+    <div class='note'>
+      <p class='noteText'>
+      {{text}}
+      </p>
+      <div class='noteButtons'>
+        <button (click)='updateText()' class='changeText'><i class='fa fa-pencil'></i></button>
+        <button class='deleteNode'><i class='fa fa-trash'></i></button>
+      </div>
+    </div>
+  `,
+})
+export class Note{
+
+  text: string = 'Note text...';
+
+  updateText(){
+    this.text = window.prompt('Note: ');
+  }
+
+}
--- noteApp/noteApp.ts
+++ noteApp/noteApp.ts
@@ -1,13 +1,19 @@
 import {Component} from 'angular2/core';
 import {Header} from './header';
+import {Note} from './note';
 
 @Component({
   selector: 'note-app',
   template: `
     <na-header></na-header>
+    <div id='noteBoard'>
+      <na-note> </na-note>
+    </div>
   `,
-  directives: [Header]
+  directives: [Header, Note]
 })
 export class noteApp {
+
+  notes: string[] = ['testing', 'this is a test', 'this is yet another test'];
 
 }
